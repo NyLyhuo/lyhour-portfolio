5a99972409f23cef251f3c46dcfb60859cf3d8c8
nylyhour <lyhuony@gmail.com>
2025-07-29 20:48:25 +0700

    update info

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,8 +1,7 @@
 export const skills = [
-    { name: "Javascript", icon: ["fab", "js"] },
-    { name: "TailWind CSS", icon: ["fab", "css3"] },
-    { name: "Vue.js", icon: ["fab", "vuejs"] },
-    { name: "PHP", icon: ["fab", "php"] },
-    { name: "Laravel", icon: ["fab", "laravel"] },
-    {name: "Git", icon: ["fab", "git-alt"] },
+  { name: "Javascript", icon: ["fab", "js"] },
+  { name: "TailWind CSS", icon: ["fab", "css3"] },
+  { name: "Vue.js", icon: ["fab", "vuejs"] },
+  { name: "Laravel", icon: ["fab", "laravel"] },
+  { name: "Git", icon: ["fab", "git-alt"] },
 ];
